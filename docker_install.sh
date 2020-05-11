0d6e16ed3bb25c579ac7ce7e5e7e52e0cff9d9a4
DongOnee <dongwon5520@gmail.com>
2020-05-11 19:45:05 +0900

    ADD remark

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 
+# install docker in docker
 apt-get update && \
 apt-get -y install --no-install-recommends apt-utils \
     apt-transport-https \
@@ -16,17 +17,4 @@ add-apt-repository \
     $(lsb_release -cs) \
     stable" && \
 apt-get update && \
-apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-
-# apt-get update;
-# apt-get install -y --no-install-recommends apt-utils;
-# apt-get install -y apt-transport-https ca-certificates curl gnupg2 zip unzip software-properties-common;
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
-# apt-get update;
-# apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce
